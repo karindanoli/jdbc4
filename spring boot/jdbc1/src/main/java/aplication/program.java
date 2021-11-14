99d09e25ab582c1855b4312ba1c74baf462338ae
Karin <karindanoli@gmail.com>
2021-11-14 17:34:29 -0300

    deletando dados jdbc

--- spring boot/jdbc1/src/main/java/aplication/program.java
+++ spring boot/jdbc1/src/main/java/aplication/program.java
@@ -1,53 +1,35 @@
 package aplication;
 
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class program {
 
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy");
         Connection conn = null;
         PreparedStatement st = null;
-        try {
+        try{
             conn = DB.getConnection();
-
             st = conn.prepareStatement(
-                    "INSERT INTO seller"
-                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                    + "VALUES "
-                    + "(?,?,?,?,?)",
-                    Statement.RETURN_GENERATED_KEYS);
-            //interrogação é um local onde posso preencher
-            st.setString(1,"Carl Purple");
-            st.setString(2,"Carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1092").getTime()));
-            st.setDouble(4,3000);
-            st.setInt(5,4);
+                    "UPDATE seller"
+                    + "SET baseSalary = BaseSalary + ? " //O QUE VAI SER ADICIONADO
+                    + "WHERE "//QUANDO FOR ATUALIZAR TEM Q COLOCAR O WHERE senão vc atualiza tudo
+                    +"(DepartmentId = ? ");
+
+            st.setDouble(1,200);
+            st.setInt(2,2);
 
             int rowsAffected = st.executeUpdate();
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done!Id = " + id);
-                }
-            }
-            else {
-                System.out.println("Done! Rows Affected: " + rowsAffected);
-            }
-            }
-        catch (SQLException e){
-            e.printStackTrace();
+            System.out.println("done rows affected: " + rowsAffected);
+            )   ;
         }
-        catch (ParseException e){
+            catch ( SQLException e){
             e.printStackTrace();
-    }
-        finally{
+            }
+            finally {
             DB.closeStatement(st);
-            DB.closeConnection();
+            DB.getConnection();
         }
-
     }}
