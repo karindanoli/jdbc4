e547ebb10b9ed01ed85518d163a4ceb3bd8bd3c6
Karin <karindanoli@gmail.com>
2021-11-14 17:13:45 -0300

    inserindo dados jdbc

--- spring boot/jdbc1/src/main/java/aplication/program.java
+++ spring boot/jdbc1/src/main/java/aplication/program.java
@@ -1,33 +1,53 @@
 package aplication;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class program {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy");
         Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement st = null;
         try {
-            conn =DB.getConnection();
-            st = conn.createStatement();//vai criar uma recuperaçao dos dados
-            rs = st.executeQuery("select + from department"); // executa a tabela
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id" + ", " + rs.getString("Name")));
+            conn = DB.getConnection();
 
+            st = conn.prepareStatement(
+                    "INSERT INTO seller"
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                    + "VALUES "
+                    + "(?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            //interrogação é um local onde posso preencher
+            st.setString(1,"Carl Purple");
+            st.setString(2,"Carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1092").getTime()));
+            st.setDouble(4,3000);
+            st.setInt(5,4);
+
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done!Id = " + id);
+                }
             }
-
-        }
+            else {
+                System.out.println("Done! Rows Affected: " + rowsAffected);
+            }
+            }
         catch (SQLException e){
             e.printStackTrace();
         }
-        finally {
+        catch (ParseException e){
+            e.printStackTrace();
+    }
+        finally{
             DB.closeStatement(st);
-            DB.closeResult(rs);
             DB.closeConnection();
-
         }
+
     }}
